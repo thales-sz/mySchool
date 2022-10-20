455127e0724f298a9b3c6aab2996c2dfc75ae185
Thales <thales.souz@outlook.com>
2022-10-20 12:32:17 -0300

    build: configurando o typescript e testando o servidor

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,12 @@
-import express from 'express';
-import 'dotenv/config';
+import express, { Request, response, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/ping', (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).json('pong');
+});
+
+export default app;
